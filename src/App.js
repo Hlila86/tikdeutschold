6d3d0404a8208fad472fdf8fab0d914143862f4b
hlila86 <hamdianoo@gmail.com>
2023-05-21 21:08:12 +0100

    react installation

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+       
+       
+          Welcome to TikDeutsch.com
+          <br/>
+          Site Under Construction ...
+   
+    </div>
+  );
+}
+
+export default App;
